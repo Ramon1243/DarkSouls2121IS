55e16a3211bba30724a1b0c0610e73be18911132
Ramon1243 <89999017+Ramon1243@users.noreply.github.com>
2021-09-23 10:14:20 -0500

    Add files via upload

---
+++ Ejercicios2.js
@@ -0,0 +1,45 @@
+var Control_Alumnos =40;
+    switch(Control_Alumnos){
+        case"Control":
+            console.log("Ya estan todos");
+        break;
+            case"Incontrol":
+                console.log("No lleganron todos");
+
+    }
+    var Perros =40;
+    switch(Perros){
+        case"Hambre":
+            console.log("tienen sed");
+        break;
+            case"Incontrol":
+                console.log("Tienen hambre");
+
+    }
+    var Placas_madre =Sirve;
+    switch(Placas){
+        case"Placas":
+            console.log("Nueva placa");
+        break;
+            case"Descompuesta":
+                console.log("Peto");
+
+    }
+    var canciones =40;
+    switch(canciones){
+        case"Ice cube":
+            console.log("esta muy chida");
+        break;
+            case"bab bunny":
+                console.log("fuchi");
+
+    }
+    var Tornillos =40;
+    switch(Tornillos){
+        case"Falta 1":
+            console.log("Noooo, busquenlo");
+        break;
+            case"Estan todos":
+                console.log("Toma el tornillo");
+
+    }
