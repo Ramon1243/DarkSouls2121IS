aa1bc2d44453089d9c639bd4a467ebe3b4cd2267
Ramon1243 <89999017+Ramon1243@users.noreply.github.com>
2021-09-23 10:14:28 -0500

    Add files via upload

---
+++ Ejercicios3.js
@@ -0,0 +1,125 @@
+var n =1;
+if(n > 10){
+    console.log("comprar dulce");
+}else{
+    switch(n){
+        
+        case 1:
+            console.log("1");
+        break;
+        case 2:
+            console.log("2");
+        break;
+        case 3:
+            console.log("3");
+        break;
+        case 4:
+            console.log("4");
+        break;
+        default:
+
+            console.log("Es menor a 1");
+        break;
+    }
+}
+
+var n =5;
+if(n > 10){
+    console.log("comprar chocolate");
+}else{
+    switch(n){
+        
+        case 1:
+            console.log("1");
+        break;
+        case 2:
+            console.log("2");
+        break;
+        case 3:
+            console.log("3");
+        break;
+        case 4:
+            console.log("4");
+        break;
+        default:
+
+            console.log("Es menor a 5");
+        break;
+    }
+}
+
+var n =2000;
+if(n > 2500){
+    console.log("comprar Perro");
+}else{
+    switch(n){
+        
+        case 1:
+            console.log("400");
+        break;
+        case 2:
+            console.log("1500");
+        break;
+        case 3:
+            console.log("1700");
+        break;
+        case 4:
+            console.log("3000");
+        break;
+        default:
+
+            console.log("Es menor a 2000");
+        break;
+    }
+}
+
+var n =15;
+if(n > 20){
+    console.log("comprar Chetos");
+}else{
+    switch(n){
+        
+        case 1:
+            console.log("13");
+        break;
+        case 2:
+            console.log("12");
+        break;
+        case 3:
+            console.log("16");
+        break;
+        case 4:
+            console.log("32");
+        break;
+        default:
+
+            console.log("Es menor a 20");
+        break;
+    }
+}
+
+
+var n =Chocolate;
+if(n > 3){
+    console.log("Tomar chocolate");
+}else{
+    switch(n){
+        
+        case 1:
+            console.log("Chocolate Negro");
+        break;
+        case 2:
+            console.log("Chocolate Pequeño");
+        break;
+        case 3:
+            console.log("Chocolate Largo");
+        break;
+        case 4:
+            console.log("Chocolate Carlos King V");
+        break;
+        default:
+
+            console.log("Tormar Chocolate comun");
+        break;
+    }
+}
