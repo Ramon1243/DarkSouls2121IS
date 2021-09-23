fd84e399124fd35d5a74ab137faf1f5b2976c520
Ramon1243 <89999017+Ramon1243@users.noreply.github.com>
2021-09-23 10:14:09 -0500

    Add files via upload

---
+++ Ejercicios.js
@@ -0,0 +1,35 @@
+//--Ejercicio 1
+var Peso =120;
+if(Peso > 120){
+        console.log("Esta muy gordo");
+}else{
+        console.log("Mantener peso");
+}
+//--Ejercicio 2
+var Perros =120;
+if(Perros > 120){
+        console.log("Necesitamos mucha comida");
+}else{
+        console.log("Mantener comida asi");
+}
+//--Ejercicio 3
+var Palabras =120;
+if(Palabras > 120){
+        console.log("Exediste el limite de palabras");
+}else{
+        console.log("Enviar Mensaje");
+}
+//--Ejercicio 4
+var Dados =12;
+if(Dados > 11){
+        console.log("Sacaste 12");
+}else{
+        console.log("Mala suerte");
+}
+//--Ejercicio 5
+var Precio =4000;
+if(Peso > 4000){
+        console.log("Esta muy caro");
+}else{
+        console.log("Comprar");
+}
