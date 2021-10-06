7493e063c389b8e02b8d5acc20a00059778208d2
Ramon1243 <89999017+Ramon1243@users.noreply.github.com>
2021-10-06 16:08:09 -0500

    Add files via upload

---
+++ Tortillas.js
@@ -0,0 +1,17 @@
+var transporte ="caminando"
+
+console.log("paso 1 salir a la entrada de mi casa");
+
+if(transporte=="caminando"){
+    console.log("paso 2: ir a la tortilleria");
+
+
+
+}else{
+    console.log("paso 2: pedir un kilo de tortillas");
+
+}
+
+console.log("paso 3 : pagar el kilo de tortillas");
+console.log("paso 4 : recibir cambio");
+console.log("paso 5: regresar a casa")
