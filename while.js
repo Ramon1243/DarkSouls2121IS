d3ecbf521475a17ef0a156c0cf6271563a3567f5
Ramon1243 <89999017+Ramon1243@users.noreply.github.com>
2021-10-06 16:49:45 -0500

    Add files via upload

---
+++ while.js
@@ -0,0 +1,22 @@
+var y = 0; 
+ while(y <= 10){
+     console.log(x);    
+     x++; 
+ }
+
+ var i = 1;
+ while(i<10){
+  console.log(i);
+  i=i*3;
+ }
+
+ var i=0; 
+ while (i<=5) { 
+     console.log(+ i);
+     i=i+1;
+ }
+ var numero = 1;
+ while( numero < 21 ) {
+   console.log(numero);
+   numero++;
+ }
